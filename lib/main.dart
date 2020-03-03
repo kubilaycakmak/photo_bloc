bc7d895080fd68fea257344c985eddd11ba8b8cf
Kubilay Çakmak <31804832+kubilayckmk@users.noreply.github.com>
2020-03-03 20:49:49 +0300

    upcoming page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,57 +11,52 @@ import 'package:photo_bloc/ui/pages/category_page.dart';
 import 'package:photo_bloc/ui/pages/fav_page.dart';
 import 'ui/home/home_page.dart';
 
-class SimpleBlocDelegate extends BlocDelegate{
-  void onTransition(Bloc bloc, Transition transition){
+class SimpleBlocDelegate extends BlocDelegate {
+  void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
     print(transition);
   }
 }
- 
+
 void main() {
   BlocSupervisor.delegate = SimpleBlocDelegate();
-
-  final HitsRepository repository = HitsRepository(
-    hitsApiClient: HitsApiClient(
-      httpClient: http.Client()
-    )
-  );
-  runApp(MyApp(
-    repository: repository
-  )); 
+  final HitsRepository repository =
+      HitsRepository(hitsApiClient: HitsApiClient(httpClient: http.Client()));
+  runApp(MyApp(repository: repository));
 }
- 
+
 class MyApp extends StatefulWidget {
-  final  HitsRepository repository;
-  const MyApp({Key key, this.repository}) : assert(repository != null), super(key: key);
+  final HitsRepository repository;
+  const MyApp({Key key, this.repository})
+      : assert(repository != null),
+        super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
   int selectedIndex = 0;
-  List children = [
-    HomePage(),
-    CategoryPage(),
-    FavoritePage()
-  ];
+  List children = [HomePage(), CategoryPage(), FavoritePage()];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
-        backgroundColor: Color(0xFFADADAD),
+        backgroundColor: Color(0xFFECECEC),
         body: BlocProvider(
           create: (context) => PhotoBloc(repository: widget.repository),
           child: children[selectedIndex],
         ),
         extendBody: true,
         bottomNavigationBar: FloatingNavbar(
-          backgroundColor: Color(0xB6777777),
+          backgroundColor: Color(0xB63D3D3D),
           selectedItemColor: Colors.white,
           selectedBackgroundColor: Colors.white10,
           unselectedItemColor: Colors.white70,
+          unselectedBackgroundColor: Colors.transparent,
           fontSize: 10,
           iconSize: 28,
           currentIndex: selectedIndex,
@@ -69,19 +65,19 @@ class _MyAppState extends State<MyApp> {
               selectedIndex = val;
             });
           },
-          items:[
+          items: [
             FloatingNavbarItem(
-              icon: selectedIndex == 0 ? Icons.home : CupertinoIcons.home, 
-              title: 'Latest'
-            ),
+                icon: selectedIndex == 0 ? Icons.home : CupertinoIcons.home,
+                title: 'Latest'),
             FloatingNavbarItem(
-              icon: selectedIndex == 1 ? CupertinoIcons.collections_solid : CupertinoIcons.collections, 
-              title: 'Category'
-            ),
+                icon: selectedIndex == 1
+                    ? CupertinoIcons.collections_solid
+                    : CupertinoIcons.collections,
+                title: 'Category'),
             FloatingNavbarItem(
-              icon: selectedIndex == 2 ? Icons.favorite: Icons.favorite_border, 
-              title: 'Favorite'
-            ),
+                icon:
+                    selectedIndex == 2 ? Icons.favorite : Icons.favorite_border,
+                title: 'Favorite'),
           ],
         ),
       ),
--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,10 +1,13 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:photo_bloc/ui/global/photo/bloc/photo_bloc.dart';
-
+import 'package:photo_view/photo_view.dart';
+import 'package:slide_popup_dialog/slide_popup_dialog.dart' as slideDialog;
 import '../global/photo/bloc/photo_bloc.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,6 +17,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String typeOfPhoto = 'latest';
+  double dotIndex = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +28,21 @@ class _HomePageState extends State<HomePage> {
         setState(() {
           if (value == 0) {
             typeOfPhoto = 'latest';
-          } else {
+            dotIndex = 0.0;
+          } else if (value == 1) {
             typeOfPhoto = 'popular';
+            dotIndex = 1.0;
+          } else {
+            typeOfPhoto = 'upcoming';
+            dotIndex = 2.0;
           }
           print('$typeOfPhoto == $value');
         });
       },
       children: <Widget>[
         buildBlocBuilder('Latest'),
-        buildBlocBuilder('Popular')
+        buildBlocBuilder('Popular'),
+        buildBlocBuilder('Upcoming')
       ],
     );
   }
@@ -40,72 +50,113 @@ class _HomePageState extends State<HomePage> {
   BlocBuilder<PhotoBloc, PhotoState> buildBlocBuilder(String title) {
     BlocProvider.of<PhotoBloc>(context).add(FetchHits(typeOfPhoto));
     final Shader linearGradient = LinearGradient(
-        colors: <Color>[Colors.teal[900], Colors.teal[200]],
-      ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 700.0));
+      colors: <Color>[Colors.teal[900], Colors.teal[200]],
+    ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 700.0));
     return BlocBuilder<PhotoBloc, PhotoState>(
-    builder: (context, state) {
-      if (state is HitsEmpty) {
-        print('in bloc builder : $typeOfPhoto');
-      }
-      if (state is HitsError) {
-        return Center(
-          child: Text('failed to fetch photos'),
-        );
-      }
-      if (state is HitsLoaded) {
-        return ListView(
-          shrinkWrap: false,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              child: Text(
-                title,
-                style: GoogleFonts.kaushanScript(
-                  fontSize: 50,
-                  foreground: Paint()..shader = linearGradient
-                ),
-              ),
-            ),
-            StaggeredGridView.countBuilder(
-              scrollDirection: Axis.vertical,
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              staggeredTileBuilder: (index) =>
-              StaggeredTile.count(2, index.isEven ? 2 : 3), 
-              crossAxisCount: 4,
-              mainAxisSpacing: 2.0,
-              crossAxisSpacing: 2.0,
-              itemCount: state.hits.length,
-              itemBuilder: (context, index) {
-                return AnimationConfiguration.staggeredGrid(
-                  duration: const Duration(milliseconds: 375),
-                  position: index,
-                  columnCount: 3,
-                  child: SlideAnimation(
-                    verticalOffset: 50.0,
-                    child: InkWell(
-                      onTap: (){
-                        print(state.hits[index].id);
-                      },
-                      child: FadeInAnimation(
-                        child: Image.network(
-                          '${state.hits[index].webUrl}',
-                          alignment: Alignment.center,
-                          fit: BoxFit.cover,
-                        ),
+      builder: (context, state) {
+        if (state is HitsEmpty) {
+          print('in bloc builder : $typeOfPhoto');
+        }
+        if (state is HitsError) {
+          return Center(
+            child: Text('failed to fetch photos'),
+          );
+        }
+        if (state is HitsLoaded) {
+          return ListView(
+            shrinkWrap: false,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    child: Text(
+                      title,
+                      style: GoogleFonts.kaushanScript(
+                          fontSize: 50,
+                          foreground: Paint()..shader = linearGradient),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: DotsIndicator(
+                      dotsCount: 3,
+                      position: dotIndex,
+                      decorator: DotsDecorator(
+                        activeColor: Colors.tealAccent,
+                        color: Colors.teal,
+                        size: const Size.square(5.0),
+                        activeSize: const Size(18.0, 9.0),
+                        activeShape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                style: BorderStyle.solid, color: Colors.black),
+                            borderRadius: BorderRadius.circular(5.0)),
                       ),
-                    )
-                  )
-                );
-              },
-            ),
-          ],
-        );
-      }
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    },
-  );
+                    ),
+                  ),
+                ],
+              ),
+              StaggeredGridView.countBuilder(
+                scrollDirection: Axis.vertical,
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                staggeredTileBuilder: (index) =>
+                    StaggeredTile.count(2, index.isEven ? 2 : 3),
+                crossAxisCount: 4,
+                mainAxisSpacing: 2.0,
+                crossAxisSpacing: 2.0,
+                itemCount: state.hits.length,
+                itemBuilder: (context, index) {
+                  return AnimationConfiguration.staggeredGrid(
+                      duration: const Duration(milliseconds: 375),
+                      position: index,
+                      columnCount: 3,
+                      child: SlideAnimation(
+                          verticalOffset: 500.0,
+                          duration: Duration(milliseconds: 1000),
+                          child: Container(
+                            child: InkWell(
+                              onTap: () {
+                                slideDialog.showSlideDialog(
+                                  context: context,
+                                  child: PhotoView(
+                                    filterQuality: FilterQuality.high,
+                                    basePosition: Alignment.center,
+                                    initialScale:
+                                        PhotoViewComputedScale.contained * 1.4,
+                                    maxScale:
+                                        PhotoViewComputedScale.contained * 2,
+                                    minScale:
+                                        PhotoViewComputedScale.contained * 1,
+                                    tightMode: false,
+                                    imageProvider: NetworkImage(
+                                        state.hits[index].largeUrl,
+                                        scale: 0.1),
+                                  ),
+                                );
+                              },
+                              child: FadeInAnimation(
+                                child: Image.network(
+                                  '${state.hits[index].webUrl}',
+                                  alignment: Alignment.center,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          )));
+                },
+              ),
+            ],
+          );
+        }
+
+        return Center(
+            child: SpinKitSquareCircle(
+          color: Colors.black,
+          size: 50.0,
+        ));
+      },
+    );
   }
 }
